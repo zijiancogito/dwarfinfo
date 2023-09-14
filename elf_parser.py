a3def68bd596f38ec70996e2764e9a8e9a1b9159
zijiancogito <345981440@qq.com>
2023-09-14 11:28:23 +0800

    update

--- elf_parser.py
+++ elf_parser.py
@@ -22,7 +22,7 @@ def parse_dwarf(filename, address=None):
             return 1, file, line
         else:
             line_map = decode_all_file_line(dwarfinfo)
-            # print(line_map)
+            print(line_map)
             return 0, line_map, None
         
 def decode_file_line(dwarfinfo, address):
